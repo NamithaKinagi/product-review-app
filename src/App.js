59d8b4e21f11caa298d5829dc6109b9fb7bea141
Namitha <namithakinagi@gmail.com>
2025-11-20 16:37:23 +0530

    Product Review App

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
+import Header from './components/Header';
+import ProductReviewList from './components/ProductReviewList';
+import ProductReviewStats from './components/ProductReviewStats';
+import ProductData from './data/ProductData';
+import ReviewForm from './components/ReviewForm';
+function App() {
+  const [review, setReview] = useState(ProductData);
+  const addReview = newReview => {
+    newReview.id = uuidv4();
+    setReview([newReview, ...review]);
+  };
+  const deleteFeedback = id => {
+   if (window.confirm('Are you sure you want to delete?')) {
+    setReview(review.filter(item => item.id !== id));
+    }
+ };
+  return (
+   <>
+   <Header />
+    <div className="container">
+    <ReviewForm handleAdd={addReview} />
+   <ProductReviewStats review={review} />
+  <ProductReviewList 
+   review={review} 
+   handleDelete={deleteFeedback} 
+  />
+  </div>
+ </>
+ );
+}
+export default App;
